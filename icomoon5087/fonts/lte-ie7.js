2693ce93ade9664f4088193d6e741a7a2bb40d71
karrikas <karrikas@gmail.com>
2013-03-19 00:07:56 +0100

    s

---
+++ icomoon5087/fonts/lte-ie7.js
@@ -0,0 +1,38 @@
+/* Load this script using conditional IE comments if you need to support IE 7 and IE 6. */
+
+window.onload = function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+			'icon-phone' : '&#xe000;',
+			'icon-envelop' : '&#xe001;',
+			'icon-location' : '&#xe002;',
+			'icon-grin' : '&#xe003;',
+			'icon-checkmark' : '&#xe004;',
+			'icon-menu' : '&#xe005;',
+			'icon-twitter' : '&#xe006;',
+			'icon-facebook' : '&#xe007;',
+			'icon-google-plus' : '&#xe008;',
+			'icon-github' : '&#xe009;',
+			'icon-wordpress' : '&#xe00a;',
+			'icon-linkedin' : '&#xe00b;',
+			'icon-menu-2' : '&#xe00c;',
+			'icon-rocket' : '&#xe00d;'
+		},
+		els = document.getElementsByTagName('*'),
+		i, attr, html, c, el;
+	for (i = 0; i < els.length; i += 1) {
+		el = els[i];
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+};
